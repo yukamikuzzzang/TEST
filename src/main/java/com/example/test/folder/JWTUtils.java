c5c3610dfafa83d1e69f7933928938f308c79cd0
yukamikuzzzang <43258236+yukamikuzzzang@users.noreply.github.com>
2023-10-27 20:35:36 +0900

    Initial commit

---
+++ src/main/java/com/example/test/folder/JWTUtils.java
@@ -0,0 +1,2 @@
+package com.example.test.folder;public class JWTUtils {
+}
---
+++ src/main/java/com/example/test/folder/vo/User.java
@@ -0,0 +1,35 @@
+package com.example.test.folder.dto;
+
+public class User {
+    private String username;
+    private String password;
+    //private String email;
+
+    User (final String username, final String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+
+        return null;
+    }
+
+    public Object getPassword() {
+
+        return null;
+    }
+
+    public void setPassword(Object password) {
+    }
+
+    public Object getEmail() {
+        return null;
+    }
+
+    public void setEmail(Object email) {
+    }
+
+
+    // Constructors, getters, and setters
+}
